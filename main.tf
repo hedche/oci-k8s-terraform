90b2244b44bd1b245257438795141f0c153a8568
Monty Rhodes <montyrhodes8@gmail.com>
2025-10-29 22:01:12 +0000

    updating oci provider to v7 rather than v5

--- main.tf
+++ main.tf
@@ -156,18 +156,6 @@ data "oci_identity_availability_domains" "ads" {
   compartment_id = var.tenancy_ocid # Changed to tenancy_ocid as ADs are at tenancy level
 }
 
-# Get available k8s versions
-data "oci_containerengine_cluster_option" "cluster_options" {
-    cluster_option_id = "all"
-    compartment_id    = var.compartment_ocid
-}
-
-# Get OKE node pool options
-data "oci_containerengine_node_pool_option" "node_pool_options" {
-    node_pool_option_id = "all"
-    compartment_id      = var.compartment_ocid
-}
-
 # Get Oracle Linux image for node pool
 data "oci_core_images" "node_pool_images" {
     compartment_id = var.tenancy_ocid
@@ -177,36 +165,35 @@ data "oci_core_images" "node_pool_images" {
     sort_by = "TIMECREATED"
     sort_order = "DESC"
 
-    # Look for the latest Oracle Linux 8.10 ARM image
     filter {
         name = "display_name"
-        values = ["Oracle-Linux-8.10-aarch64-2025.09.16-0"]
-        regex = false
+        values = ["Oracle-Linux-8.10-aarch64-2025.*"]
+        regex = true
     }
 }
 
-# Output debugging information
-output "debug_info" {
-  value = {
-    node_pool_options = data.oci_containerengine_node_pool_option.node_pool_options
-    selected_image    = data.oci_core_images.node_pool_images.images[*]
-  }
+# Essential outputs
+output "cluster_id" {
+  description = "OCID of the created OKE cluster"
+  value       = oci_containerengine_cluster.k8s_cluster.id
 }
 
-# Output IAM information
-output "iam_info" {
-  value = {
-    group_name = oci_identity_group.k8s_group.name
-    group_id   = oci_identity_group.k8s_group.id
-    policies   = {
-      network = oci_identity_policy.k8s_network_policy.name
-      cluster = oci_identity_policy.k8s_cluster_policy.name
-      compute = oci_identity_policy.k8s_compute_policy.name
-      lb      = oci_identity_policy.k8s_lb_policy.name
-    }
-  }
+output "node_pool_id" {
+  description = "OCID of the created node pool"
+  value       = oci_containerengine_node_pool.k8s_node_pool.id
 }
 
-output "available_kubernetes_versions" {
-    value = data.oci_containerengine_cluster_option.cluster_options.kubernetes_versions
+output "kubernetes_version" {
+  description = "Version of Kubernetes running on the cluster"
+  value       = oci_containerengine_cluster.k8s_cluster.kubernetes_version
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for your Kubernetes API server"
+  value       = oci_containerengine_cluster.k8s_cluster.endpoints[0].public_endpoint
+}
+
+output "vcn_id" {
+  description = "OCID of the VCN created for the cluster"
+  value       = oci_core_vcn.k8s_vcn.id
 }
--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     oci = {
       source  = "oracle/oci"
-      version = "~> 5.0.0"  # Latest stable version as of Oct 2025
+      version = "> 5.0.0"  # Latest stable version as of Oct 2025
     }
   }
 }
